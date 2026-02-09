e409b19280d766d58cbf7081709e2d16b87f19d1
Eder Andres Avila Nino <ederandresan@gmail.com>
2026-02-08 19:33:42 -0500

    chapter 09 progress

---
+++ ch03/ruby/gear_3.rb
@@ -0,0 +1,40 @@
+class Wheel
+  attr_reader :rim, :tire
+
+  def initialize(rim, tire)
+    @rim = rim
+    @tire = tire
+  end
+
+  def diameter
+    rim + (tire * 2)
+  end
+
+  def circumference
+    diameter * Math::PI
+  end
+end
+
+class Gear
+  attr_reader :chainring, :cog, :wheel
+
+  def initialize(chainring:, cog:, wheel:)
+    @chainring = chainring
+    @cog = cog
+    @wheel = wheel
+  end
+
+  def ratio
+    chainring / cog.to_f
+  end
+
+  def gear_inches
+    ratio * wheel.diameter
+  end
+end
+
+gear_1 = Gear.new(
+  :cog => 11,
+  :chainring => 52,
+  :wheel => Wheel.new(26, 1.5))
+puts "Gear inches:\t#{gear_1.gear_inches}"
---
+++ ch03/ruby/gear_a.rb
@@ -0,0 +1,38 @@
+class Wheel
+  attr_reader :rim, :tire
+
+  def initialize(rim, tire)
+    @rim = rim
+    @tire = tire
+  end
+
+  def diameter
+    rim + (tire * 2)
+  end
+
+  def circumference
+    diameter * Math::PI
+  end
+end
+
+class Gear
+  attr_reader :chainring, :cog, :rim, :tire
+
+  def initialize(chainring, cog, rim, tire)
+    @chainring = chainring
+    @cog = cog
+    @rim = rim
+    @tire = tire
+  end
+
+  def ratio
+    chainring / cog.to_f
+  end
+
+  def gear_inches
+    ratio * Wheel.new(rim, tire).diameter
+  end
+end
+
+gear_1 = Gear.new(52, 11, 26, 1.5)
+puts "Gear inches:\t#{gear_1.gear_inches}"
